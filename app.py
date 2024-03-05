6bd69c8e6c5e466726892d1d4da4bda57de6aaeb
qwerty7989 <44471969+qwerty7989@users.noreply.github.com>
2024-03-05 23:25:28 +0700

    Rename variable in app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request
-import tensorflow as tf
 from transformers import BertTokenizer, TFBertModel
-import re
-import pandas as pd
+import tensorflow as tf
 import numpy as np
+import pandas as pd
+import re
 from time import time
-
 
 app = Flask(__name__)
 
 with app.app_context():
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        model = TFBertModel.from_pretrained('bert-base-uncased')
-        model_classification = tf.keras.models.load_model('model/bert_model.h5',custom_objects={'TFBertModel':model})
+        BERT_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        BERT_model = TFBertModel.from_pretrained('bert-base-uncased')
+        BERT_classification = tf.keras.models.load_model('model/bert_model.h5', custom_objects={'TFBertModel': BERT_model})
 
 @app.route("/")
 def index():
@@ -20,20 +19,24 @@ def index():
 
 @app.route("/result")
 def result():
-        start = time()
-        input_text = request.args.get('text')
+        start_time = time()
 
+        # Recieve & Clean data
+        input_text = request.args.get('text')
         input_text = re.sub(r'\n+', ' ', input_text)
+
+        # Encoding data
         id_text = []
         mask_text = []
+        BERT_INP = BERT_tokenizer.encode_plus(input_text, add_special_tokens=True, max_length=512, padding='max_length', return_attention_mask=True, truncation=True)
 
-        bert_inp = tokenizer.encode_plus(input_text, add_special_tokens = True, max_length = 512, padding = 'max_length', return_attention_mask = True, truncation = True)
-        id_text.append(bert_inp['input_ids'])
-        mask_text.append(bert_inp['attention_mask'])
+        id_text.append(BERT_INP['input_ids'])
+        mask_text.append(BERT_INP['attention_mask'])
 
         id_text = np.array(id_text)
         mask_text = np.array(mask_text)
 
-        result = model_classification.predict([id_text, mask_text])
+        # Predict
+        result = BERT_classification.predict([id_text, mask_text])
 
-        return f'You enter {input_text} and it is probably {result} and spent time {time()-start:.3f}'
+        return f'You enter {input_text} and it is probably {result} and spent time {time()-start_time:.3f}'
