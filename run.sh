55ed592cb32ba09ed38390a3b352844e279fcc9b
qwerty7989 <44471969+qwerty7989@users.noreply.github.com>
2024-03-10 22:35:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@ apt install python3.11-venv
 python3 -m venv .venv
 . .venv/bin/activate
 pip install -r requirements.txt
+flask --app app.py run --host=0.0.0.0
 
 # Prod
-gunicorn -w 1 -b 0.0.0.0 'app:app' --daemon
+gunicorn -w 1 -b 0.0.0.0 'app:app' --timeout 300 --daemon
 ps -ef | grep gunicorn
 kill -9 <pid>
