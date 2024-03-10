d01a018cbcca9b2729849c99ec3c67902a9b3164
qwerty7989 <44471969+qwerty7989@users.noreply.github.com>
2024-03-10 22:03:07 +0700

    Add Deberta and Roberta

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from transformers import BertTokenizer, TFBertModel
+from transformers import AutoTokenizer, TFAutoModel
+import sentencepiece
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -9,9 +10,18 @@ from time import time
 app = Flask(__name__)
 
 with app.app_context():
-        BERT_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        BERT_model = TFBertModel.from_pretrained('bert-base-uncased')
-        BERT_classification = tf.keras.models.load_model('model/bert_model.h5', custom_objects={'TFBertModel': BERT_model})
+        # Load Model
+        BERT_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        BERT_model = TFAutoModel.from_pretrained('bert-base-uncased')
+        BERT_classification = tf.keras.models.load_model('model/bert_model_auto.h5', custom_objects={'TFBertModel': BERT_model})
+
+        DEBERTA_tokenizer = AutoTokenizer.from_pretrained('microsoft/deberta-v3-small', use_fast=False)
+        DEBERTA_model = TFAutoModel.from_pretrained('microsoft/deberta-v3-small')
+        DEBERTA_classification = tf.keras.models.load_model('model/deberta_model_7.h5', custom_objects={'TFDebertaV2Model': DEBERTA_model})
+
+        ROBERTA_tokenizer = AutoTokenizer.from_pretrained('FacebookAI/roberta-base')
+        ROBERTA_model = TFAutoModel.from_pretrained('FacebookAI/roberta-base')
+        ROBERTA_classification = tf.keras.models.load_model('model/roberta_model_2.h5', custom_objects={'TFRobertaModel': ROBERTA_model})
 
 @app.route("/")
 def index():
@@ -25,18 +35,32 @@ def result():
         input_text = request.args.get('text')
         input_text = re.sub(r'\n+', ' ', input_text)
 
+        # Recieve chosen model
+        chosen_model = request.args.get('model')
+
         # Encoding data
         id_text = []
         mask_text = []
-        BERT_INP = BERT_tokenizer.encode_plus(input_text, add_special_tokens=True, max_length=512, padding='max_length', return_attention_mask=True, truncation=True)
 
-        id_text.append(BERT_INP['input_ids'])
-        mask_text.append(BERT_INP['attention_mask'])
+        if chosen_model == "bert":
+                MODEL_INP = BERT_tokenizer.encode_plus(input_text, add_special_tokens=True, max_length=512, padding='max_length', return_attention_mask=True, truncation=True)
+        elif chosen_model == "deberta":
+                MODEL_INP = DEBERTA_tokenizer.encode_plus(input_text, add_special_tokens=True, max_length=512, padding='max_length', return_attention_mask=True, truncation=True)
+        elif chosen_model == "roberta":
+                MODEL_INP = ROBERTA_tokenizer.encode_plus(input_text, add_special_tokens=True, max_length=512, padding='max_length', return_attention_mask=True, truncation=True)
+
+        id_text.append(MODEL_INP['input_ids'])
+        mask_text.append(MODEL_INP['attention_mask'])
 
         id_text = np.array(id_text)
         mask_text = np.array(mask_text)
 
         # Predict
-        result = BERT_classification.predict([id_text, mask_text])
+        if chosen_model == "bert":
+                result = BERT_classification.predict([id_text, mask_text])
+        elif chosen_model == "deberta":
+                result = DEBERTA_classification.predict([id_text, mask_text])
+        elif chosen_model == "roberta":
+                result = ROBERTA_classification.predict([id_text, mask_text])
 
         return f'You enter {input_text} and it is probably {result} and spent time {time()-start_time:.3f}'
