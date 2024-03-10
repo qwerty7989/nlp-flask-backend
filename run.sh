5f2af7ab4435c1dd0531596ec40cf4b5912a8ecc
qwerty7989 <44471969+qwerty7989@users.noreply.github.com>
2024-03-10 22:12:08 +0700

    Update requirement.txt

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
+# Dev
 apt install python3.11-venv
 python3 -m venv .venv
 . .venv/bin/activate
 pip install -r requirements.txt
+
+# Prod
+gunicorn -w 1 -b 0.0.0.0 'app:app' --daemon
+ps -ef | grep gunicorn
+kill -9 <pid>
